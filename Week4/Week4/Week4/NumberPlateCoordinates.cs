5f7f4cf93d43b839a05e05d94f4b9ff7bae55f7a
Erwin <dicehead3@gmail.com>
2013-03-10 23:12:16 +0100

    Started with the new project for this week
    Created a new project in a separate folder and put a class in it for
    transfering the co-ordinates of the corners of the licenceplate

---
+++ Week4/Week4/Week4/NumberPlateCoordinates.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4
+{
+    public class NumberPlateCoordinates
+    {
+        public Point LeftTop { get; set; }
+        public Point RightTop { get; set; }
+        public Point LeftBottom { get; set; }
+        public Point RightBottom { get; set; }
+
+        public string FileName { get; set; }
+    }
+}
